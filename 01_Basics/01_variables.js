8ff97712a77f193edbde205c835c7c847f9121fe
PrathamSaha12 <126253267+PrathamSaha12@users.noreply.github.com>
2025-06-22 07:39:46 +0000

    datatatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId =144553
+let accountEmail = "prathamsaha15@gmail.com"
+var accountPassword = "12345"
+accountCity ="Siliguri"
+let accountState;
+
+//accountId is a constant it cant change
+//ccountId=2
+//prefer not not use var because of issue in block scope and functional scope
+
+accountEmail = "hello@gmail.com"
+accountPassword ="23456"
+accountCity = "kolkata"
+console.table ([accountId,accountEmail,accountPassword,accountCity,accountState]);
