ab53893fc8b1132fe7b390eab4a74c375ca0e55c
PrathamSaha12 <126253267+PrathamSaha12@users.noreply.github.com>
2025-07-10 16:47:07 +0000

    objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,42 @@
+//const tinderUser = new Object()//singletont 
+
+
+const tinderUser ={}
+tinderUser.id = "1234455",
+tinderUser.name = "Pratham",
+tinderUser.isLoggedin = false
+
+//console.log(tinderUser) 
+
+  const regularuser ={
+    email:"prathamsaha15@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"pratham",
+            lastname:"saha"
+        }
+    }
+}
+
+//console.log(regularuser.fullname.userfullname.firstname);
+
+
+const obj1 ={1:"A",2:"B"};
+const obj2 ={1:"C",2:"D"};
+
+//const obj3 = Object.assign({},obj1,obj2);//this is used to combine objects
+//console.log(obj3);
+const obj3 ={...obj1,...obj2}//this is more preferable
+console.log(obj3);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty("name"));
+
+
+
+
+
+
