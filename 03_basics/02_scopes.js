ff0248a58114451e12f57d823ccada2ffbaddcb4
PrathamSaha12 <126253267+PrathamSaha12@users.noreply.github.com>
2025-07-11 18:19:03 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,10 @@
+if(true){
+    let a = 30;
+    const b =40;
+    var c = 50;
+}
+
+console.log(a);
+console.log(b);
+console.log(c);//var will be print outside of the scope but a and b will not be print
+
