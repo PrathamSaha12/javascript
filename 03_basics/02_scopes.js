295b944940c1088c5734220a24d5ea19992d52cb
PrathamSaha12 <126253267+PrathamSaha12@users.noreply.github.com>
2025-07-12 13:20:28 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,7 +4,31 @@ if(true){
     var c = 50;
 }
 
-console.log(a);
-console.log(b);
-console.log(c);//var will be print outside of the scope but a and b will not be print
+//console.log(a);
+//console.log(b);
+//console.log(c);//var will be print outside of the scope but a and b will not be print
+
+function one (){
+    const username = "pratham"
+
+    function two(){
+        const website ="youtube"
+        console.log(username);
+    }
+    //console.log(website);
+    //two()
+    
+}
+//one()
+//******************************************************************/
+addone(5)//this will work
+function addone(num){
+    return num+1;
+}
+//****************************************************************/
+addtwo(5)//this will not work because we declare a function in a constant
+const addtwo=function(num){
+    return num+2;
+}
+
 
